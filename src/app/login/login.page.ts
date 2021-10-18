66e87297cfe74d490d41ce49dc526cbb6ffa18d2
Daniel Lucario <daniel_lucario719@outlook.com>
2021-10-18 15:59:23 -0500

    login & registro

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  formInicio: FormGroup;
+
+  constructor(public fb: FormBuilder) {
+    this.formInicio = fb.group({
+      'nombre': new FormControl("", Validators.required),
+      'password': new FormControl("", Validators.required)
+    })
+  }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/registro/registro.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.page.html',
+  styleUrls: ['./registro.page.scss'],
+})
+export class RegistroPage implements OnInit {
+
+  formRegistro: FormGroup;
+
+  constructor(public fb: FormBuilder) {
+    this.formRegistro = fb.group({
+      'nombre': new FormControl("", Validators.required),
+      'password': new FormControl("", Validators.required),
+      'confirmar': new FormControl("", Validators.required)
+    })
+  }
+
+  ngOnInit() {
+  }
+
+}
